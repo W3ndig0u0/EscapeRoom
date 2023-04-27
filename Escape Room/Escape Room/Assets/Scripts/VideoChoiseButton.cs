875233260b536756577b90250addb4db7a814cd2
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2023-04-27 19:01:33 +0200

    Lade in storyboard

--- Escape Room/Escape Room/Assets/Scripts/VideoChoiseButton.cs
+++ Escape Room/Escape Room/Assets/Scripts/VideoChoiseButton.cs
@@ -43,6 +43,11 @@ public class VideoChoiseButton : MonoBehaviour
         btnParent.SetActive(true);
     }
 
+    public void ShowStoryboard(GameObject storyBoardImg)
+    {
+        storyBoardImg.SetActive(true);
+    }
+
 
 
 
