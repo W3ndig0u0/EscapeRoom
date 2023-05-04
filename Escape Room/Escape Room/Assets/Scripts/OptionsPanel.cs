14f4a06e3c0fc559fbcc15993e5da904ec86298b
linnangre <linnhelenaangre@hotmail.com>
2023-05-04 16:18:10 +0200

    OptionsPanel upgrade
    Kodningen är ej klar. Vi vet inte hur vi får knapparna i panelen att fungera.
    
    Co-Authored-By: ibenSj <131025835+ibenSj@users.noreply.github.com>

---
+++ Escape Room/Escape Room/Assets/Scripts/OptionsPanel.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsPanel : MonoBehaviour
+{
+
+    public GameObject optionsPanel;
+    public Button startButton;
+    public Button optionsButton;
+    public Button quitButton;
+   
+    public void Start()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    public void OnClickedOptionsButton()
+    {
+        optionsPanel.SetActive(true);
+        startButton.interactable = false;
+        optionsButton.interactable = false;
+        quitButton.interactable = false;
+
+    }
+
+    public void HideOptions()
+    {
+        if (optionsPanel != null)
+        {
+            optionsPanel.SetActive(false);
+            startButton.interactable = true;
+            optionsButton.interactable = true;
+            quitButton.interactable = true;
+        }
+
+    }
+
+}
